5d34ed98c4a606e83d9e0ca7bb912b776b8890a5
Max Ulanov <ulanovmaks29@gmail.com>
2024-05-30 18:04:28 +0300

    WIP

--- src/views/catalog/CatalogCategories.vue
+++ src/views/catalog/CatalogCategories.vue
@@ -14,18 +14,70 @@
             class="text-none max-sm:w-full"
             append-icon="mdi-plus"
             color="primary"
+            @click="isDialogOpen = true"
         >
             Add new category
         </v-btn>
     </div>
 
     <catalog-table :headers="headers" :items="items" />
+
+    <v-dialog
+        v-model="isDialogOpen"
+        transition="dialog-bottom-transition"
+        fullscreen
+    >
+        <v-card>
+            <v-toolbar class="mb-5" color="primary">
+                <div class="container flex items-center">
+                    <v-toolbar-title>Create new category</v-toolbar-title>
+
+                    <v-btn
+                        icon="mdi-close"
+                        @click="isDialogOpen = false"
+                    ></v-btn>
+                </div>
+            </v-toolbar>
+
+            <div class="container">
+                <h3 class="mb-5 text-lg">Please fill out the fields below</h3>
+
+                <form @submit.prevent>
+                    <p class="mb-3">Please enter a category name</p>
+
+                    <v-text-field
+                        v-model="name"
+                        name="name"
+                        label="Category name"
+                        type="name"
+                        variant="outlined"
+                        hint="Enter name of category"
+                        class="mb-2"
+                    />
+
+                    <p class="mb-3">Please choose a file of background</p>
+
+                    <v-file-input
+                        variant="outlined"
+                        label="Category background"
+                        show-size
+                        prepend-icon=""
+                    ></v-file-input>
+
+                    <v-btn color="primary text-none"> Save </v-btn>
+                </form>
+            </div>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
 
     import CatalogTable from '@/components/base/CatalogTable.vue';
+
+    const isDialogOpen = ref(false);
+    const name = ref('');
 
     const headers = ref([
         {
