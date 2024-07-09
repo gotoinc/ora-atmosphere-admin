0be873e4cf914777c2293d275bb279ba085238b3
Max Ulanov <ulanovmaks29@gmail.com>
2024-07-09 14:27:37 +0300

    Update eslint plugin and disable vue-tsc in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
