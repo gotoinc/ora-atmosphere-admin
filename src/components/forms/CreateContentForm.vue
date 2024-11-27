c2a6045eaef0876a49fee16bd91929274e8c697e
Max Ulanov <ulanovmaks29@gmail.com>
2024-11-27 14:59:40 +0200

    Update combobox for tags

--- src/components/forms/CreateContentForm.vue
+++ src/components/forms/CreateContentForm.vue
@@ -233,6 +233,7 @@
                     multiple
                     label="Tags"
                     variant="outlined"
+                    @update:model-value="splitTags"
                 >
                     <template #selection="{ item }">
                         <span class="tag tag--fill">
@@ -424,6 +425,13 @@
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         return videos.value.filter((video) => !video.language.id) ?? [];
     });
+
+    const splitTags = (items: string[]) => {
+        tags.value = items
+            .map((tag) => tag.split(','))
+            .flat()
+            .filter((tag) => tag);
+    };
 
     /*
      * Functions to manage audio
